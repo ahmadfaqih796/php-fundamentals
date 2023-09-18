a7fcfbede2d33eb9a40eeb94ed5514fc471a04d7
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-09-18 13:19:20 +0700

    handling form

--- learn/2-forms/1-form-handling/index.php
+++ learn/2-forms/1-form-handling/index.php
@@ -4,7 +4,7 @@
 
    <form action="welcome.php" method="post">
       Name: <input type="text" name="name"><br>
-      Email: <input type="text" name="email"><br>
+      E-mail: <input type="text" name="email"><br>
       <input type="submit">
    </form>
 
